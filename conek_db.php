de04c424feb308714c9d0d25e4945f8d0b597548
Rosyad03 <160685917+Rosyad03@users.noreply.github.com>
2024-04-30 15:40:34 +0700

    Create conek_db.php

---
+++ conek_db.php
@@ -0,0 +1,13 @@
+<?php
+echo "Nama saya : " .$_POST['nama'];
+echo "<br />";
+echo "Tempat lahir saya di : " .$_POST['tempatlahir'];
+echo "<br />";
+echo "Tanggal Lahir saya : " .$_POST['tanggallahir'];
+echo "<br />";
+echo "Alamat saya di : " .$_POST['alamat'];
+echo "<br />";
+echo "Jenis Kelamin saya : " .$_POST['sex'];
+echo "<br />";
+echo "Agama saya : " .$_POST['agama'];
+?>
