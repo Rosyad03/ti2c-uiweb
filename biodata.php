5412ef84142712368ce6ec6a12029a5b3ffb4e7a
Rosyad03 <160685917+Rosyad03@users.noreply.github.com>
2024-04-30 15:39:31 +0700

    Create biodata.php

---
+++ biodata.php
@@ -0,0 +1,19 @@
+<form method="post" action="proses-biodata.php"?>
+Nama           :<br><input type="textbox" id="nama" name="nama"><br>
+Tempat lahir   :<br><input type="textbox" id="tempatlahir" name="tempatlahir"><br>
+Tanggal Lahir  :<br><input type="textbox" id="tanggallahir" name="tanggallahir"><br>
+Alamat         :<br><input type="textbox" id="alamat" name="alamat"><br>
+Jenis Kelamain :<br>
+                <select name="sex">
+                <option value="laki-laki">laki-laki<br> </option>
+                <option value="peerempuan">perempuan<br> </option>
+                <select><br>
+Agama          :<br>
+                <input type="radio"id="agama" name="agama"value="islam"> <br>
+                <input type="radio"id="agama" name="kristen"value="kristen"> <br>
+                <input type="radio"id="agama" name="protestan"value="protestan"> <br>
+                <input type="radio"id="agama" name="hindu"value="hindu"> <br>
+                <input type="radio"id="agama" name="budha"value="butha"> <br>
+                <br>
+<input type="submit" name="submit" value="simpan"><input type="reset" name="reset" value="cancel">
+</form>
